7395201a21262dcfa23ef067fb5c100773c4193c
Sreehari A S <sreeharias1111@am.students.amrita.edu>
2020-05-14 01:03:55 +0530

    Commit

---
+++ app/src/main/java/com/example/canteenmanagement/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.canteenmanagement;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+
+
+        setContentView(R.layout.activity_login);
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setTextColor(Color.WHITE);
+
+    }
+}
---
+++ app/src/main/java/com/example/canteenmanagement/activity_admindash.java
@@ -0,0 +1,14 @@
+package com.example.canteenmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_admindash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admindash);
+    }
+}
